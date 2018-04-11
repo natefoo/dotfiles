5c0dac2f7e3ea28cc9144943c7f623c4a6d5faeb
Nate Coraor <nate@bx.psu.edu>
2018-04-11 10:07:58 -0400

    Automatically set up rotated layout for rotated screens

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -190,8 +190,13 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    --awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[2])
+    -- Default layout
+    local layout = awful.layout.layouts[2]
+    if s.geometry.height > s.geometry.width then
+        -- Screens that are rotated get a rotated layout
+        layout = awful.layout.layouts[4]
+    end
+    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, layout)
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()

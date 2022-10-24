3068d0339f7f7798434a748adc4e8989025beeb6
Nate Coraor <nate@bx.psu.edu>
2022-10-24 14:57:55 -0400

    Only start 1 master window in awesome, 2 is the exception for the giant screen on weyerbacher

--- .config/awesome/themes/nate/theme.lua
+++ .config/awesome/themes/nate/theme.lua
@@ -18,7 +18,7 @@ theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.master_width_factor = 0.3
-theme.master_count = 2
+theme.master_count = 1
 theme.column_count = 3
 theme.useless_gap   = 0
 theme.border_width  = 1
